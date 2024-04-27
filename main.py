04974355030b93f90f227078e9a771afae891f0b
sapphydoyle <33693806-sapphydoyle@users.noreply.replit.com>
2024-04-27 16:30:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome! This is the band name generator, lets go!")
+city = input("What is the name of the city your were born in? \n")
+food = input("What your favourite food? \n")
+print("The name for your band should be "+city+" "+food)
